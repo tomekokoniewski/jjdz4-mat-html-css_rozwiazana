acc1fddfa16183dac8808f0d3366f13da0d5a00d
tomekokoniewski <alai@wp.pl>
2017-11-25 21:01:28 +0100

    13 exerc

--- ćwiczenie13/script.js
+++ ćwiczenie13/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     console.log($('.wrapper'));
+    console.log($('.firstSection'));
+    console.log($('ul li.list_item:nth-child(3)'));
+    //console.log($('a[href="https://www.wikipedia.org/"]')); lub jeżeli http musimy użyc poniższego
+    console.log($('a[href*="wikipedia"]'));
+
 });

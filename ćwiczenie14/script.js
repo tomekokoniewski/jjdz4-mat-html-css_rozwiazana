ffecce8a530ecb0adf01851dc5d49647a375e2f2
tomekokoniewski <alai@wp.pl>
2018-01-27 13:52:54 +0100

    1

--- ćwiczenie14/script.js
+++ ćwiczenie14/script.js
@@ -1,5 +1,15 @@
+/*
 $(document).ready(function() {
     $('#button').on('click', function() {
         // Tutaj powinna znaleźć się Twoja funkcja
     });
 });
+*/
+
+$(document).ready(
+    function() {
+        $('#button').click(
+            function () {
+                $('#alert').css('background', 'green');
+            })
+    })
